8966ea10ef01372849793691bad6c10b968883a2
onah anthony <anthonyonah18@gmail.com>
2023-03-21 20:15:55 +0100

    added

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
-    bucket = "primuslearning-app"
+    bucket = "primuslearning-app123"
     region = "us-east-1"
-    key = "eks/terraform.tfstate"
+    key = "ssh-key/terraform.tfstate"
   }
 }
